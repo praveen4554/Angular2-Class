8d95d0ff09a0af5d2e6f951d3227396ce3b54fd8
Praveen <praveen4554@gmail.com>
2017-01-06 22:28:00 +0530

    First App

---
+++ firstApp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  //templateUrl: './app.component.html',
+  template:'<h1 class="tit">{{title}}</h1>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+}
